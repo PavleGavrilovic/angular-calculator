3eced5f860dfad093fecfcc32d776c5d71b1ff2e
paja <paja@hotmail.com>
2022-03-07 14:20:29 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  @ViewChild('input') input:ElementRef;
+
+  onClick(e) {
+    this.input.nativeElement.value += e.target.innerText
+  }
+
+  calculate() {
+    this.input.nativeElement.value=eval(this.input.nativeElement.value)
+  }
+
+  clear() {
+    this.input.nativeElement.value='';
+  }
 }
